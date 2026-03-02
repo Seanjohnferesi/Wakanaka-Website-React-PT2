798ea79fd51a4877b2af13bb2050ee9777bc8b1d
Seanjohnferesi <seanjohnferesi27@gmail.com>
2026-03-02 15:21:38 +0200

    feat: add header component with navigation and branding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,23 @@
+import "../styles/header.css";
+import webIcon from "../assets/icons/wakanaka-icon.png";
+
 function Header() {
     return(
-        <h1>hello</h1>
+        <header>
+            <div className="web-icon">
+                <h1>Wakanaka</h1>
+                <img src={webIcon} alt={webIcon} />
+            </div>
+
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Gallery</li>
+                    <li><a href="http://" className="book-btn">Book</a></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
