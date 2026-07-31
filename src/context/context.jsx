d102562dfec00adb17d2de6445df8ca2138f5253
Seanjohnferesi <seanjohnferesi27@gmail.com>
2026-07-31 14:36:50 +0200

    feat: add global context component for state management

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,11 +1,20 @@
 import {createContext, useState, useContext} from 'react';
 
-const GlobalContext = createContext();
+const WebsiteContext = createContext();
+export const useWebsiteContext = () => useContext(WebsiteContext);
 
-function GlobalProvider ({children}) {
-
+function WebsiteProvider ({children}) {
+    const [isVisible , setIsVisible] = useState(false);
 
     return(
-        <></>
+        <WebsiteContext.Provider value={
+            {
+                isVisible,
+                setIsVisible,
+                
+            }
+        }>
+            {children}
+        </WebsiteContext.Provider>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from "./App"
 import "./styles/global.css"
+import {WebsiteProvider} from './context/context.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <StrictMode>
-            <App />
+            <WebsiteProvider>
+                <App />
+            </WebsiteProvider>
         </StrictMode>
     </BrowserRouter>
 )
